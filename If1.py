e42a81a30d02ebc3ff74c6e5eb4fdd414b16bd2a
sanaullahaslam <sanaullahaslam09@gmail.com>
2023-10-14 15:07:21 +0500

    If another program

---
+++ If1.py
@@ -0,0 +1,4 @@
+
+if 5 > 4:
+ print("Five is Greater")   #identation is must in if condition in python otherwise it will produce error
+ print("this is for testing spaces")  #if we use multipe print func we use same space
