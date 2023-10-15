6d45c22512df1e6c5568055b5d1fde7071006e8d
sanaullahaslam <sanaullahaslam09@gmail.com>
2023-10-15 21:58:04 +0500

    Variable program

---
+++ varibaleprogram.py
@@ -0,0 +1,12 @@
+
+x = 10.0                   #in python we don't need to declare varibale datatype
+y = int(10)              # we can declare if we hae to work on some critical data
+z = "sanaullah"          # like this variable understand that the value is in string datatype
+
+print(z)
+print(y)
+print(x)
+
+print(type(z))        #type keyword is used to know about x y z variable datatype
+print(type(y))
+print(type(x))
