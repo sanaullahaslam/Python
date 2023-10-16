cb84f8c24372ad40522c3d66951036035981bff2
sanaullahaslam <sanaullahaslam09@gmail.com>
2023-10-16 18:27:46 +0500

    String Program in Python

---
+++ stringprogram1.py
@@ -0,0 +1,13 @@
+'''
+name=input("enter your good name : ")
+name=str(name)
+print("user ipnut name",name)
+print("Name in uppercase",name.upper())
+print("Name in lower case",name.lower())
+print("Value at 5th index ",name[5])
+print("find character ",name.find('n'))
+'''
+
+name= "sanaullah aslam"
+print(name)
+print(name.replace("aslam","babar"))
