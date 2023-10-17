73c5c9ce071a8fe6a946b60f80ddde4e86d13d9d
sanaullahaslam <sanaullahaslam09@gmail.com>
2023-10-17 12:42:20 +0500

    Multiplication program

---
+++ multiplication.py
@@ -0,0 +1,11 @@
+# Multiplication Program
+
+# Get input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Multiply the numbers
+result = num1 * num2
+
+# Display the result
+print("The product of {} and {} is: {}".format(num1, num2, result))
