570d63af66080073b6613c290da7d30b13e52c38
sanaullahaslam <sanaullahaslam09@gmail.com>
2023-10-17 12:39:54 +0500

    Subtraction program

---
+++ subtraction.py
@@ -0,0 +1,11 @@
+# Subtraction Program
+
+# Get input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Subtract the second number from the first
+result = num1 - num2
+
+# Display the result
+print("The result of subtracting {} from {} is: {}".format(num2, num1, result))
