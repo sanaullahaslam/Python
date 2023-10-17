1876998da2e597e166a382e9e3d0f2bf99977002
sanaullahaslam <sanaullahaslam09@gmail.com>
2023-10-17 12:38:08 +0500

    Addition program in pyhon

---
+++ addition.py
@@ -0,0 +1,11 @@
+# Addition Program
+
+# Get input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Add the numbers
+result = num1 + num2
+
+# Display the result
+print("The sum of {} and {} is: {}".format(num1, num2, result))
